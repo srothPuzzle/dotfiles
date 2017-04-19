354b175b870ec0d23036d6b1b27789ae2493fa08
Andy Pollari <pollari@gmx.ch>
2017-04-19 08:38:13 +0200

    added xrandr scripts for work displays

---
+++ link/.screenlayout/2-monitors.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off \
+  --output eDP1 --off \
+  --output DP1 --off \
+  --output HDMI2 --off \
+  --output HDMI1 --off \
+  --output DP1-3 --off \
+  --output DP1-2 --mode 1920x1200 --pos 1920x0 --rotate normal \
+  --output DP1-1 --mode 1920x1200 --pos 0x0 --rotate normal
