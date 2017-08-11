27d5f25a0e2f535ba2093bfde8c88b8e3a12b1ac
Andy Pollari <pollari@gmx.ch>
2017-08-11 08:54:25 +0200

    restart i3 in screenlayouts scripts

--- link/.screenlayout/2-monitors.sh
+++ link/.screenlayout/2-monitors.sh
@@ -7,3 +7,5 @@ xrandr --output VIRTUAL1 --off \
   --output DP1-3 --off \
   --output DP1-2 --mode 1920x1200 --pos 1920x0 --rotate normal \
   --output DP1-1 --mode 1920x1200 --pos 0x0 --rotate normal
+
+i3-msg restart
